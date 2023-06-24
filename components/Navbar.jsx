2ff8362eee6d63aec9a243bafbe007dfdfd50b2e
Martin Winther <mswinther@icloud.com>
2023-06-24 09:01:35 +0200

    Card

---
+++ components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+	return (
+		<>
+			<nav className="nav">
+				<img className="nav--logo" src="../src/assets/airbnb-logo.png" />
+			</nav>
+		</>
+	);
+}
+
+export default Navbar;
